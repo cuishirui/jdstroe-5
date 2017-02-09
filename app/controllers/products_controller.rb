fb5ae2cfe9c62549c78c7eee0a1777a262388d69
cuishirui <344494024@qq.com>
2017-02-09 15:25:19 +0800

    implement current_cart function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
 
+
   def index
     @products = Product.all
   end
@@ -11,7 +12,10 @@ class ProductsController < ApplicationController
   def add_to_cart
     @product = Product.find(params[:id])
     current_cart.add_product_to_cart(@product)
+    flash[:notice] = "成功加入购物车"
     redirect_to :back
-    
+
   end
+
+
 end
